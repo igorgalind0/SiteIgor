fff871da772050c69bf7a27be713a622ce4cf647
igorgalind0 <igorgalindo950@gmail.com>
2025-03-14 21:02:40 -0300

    atual

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ menuMobile.addEventListener('click', () => {
     ulMenu.classList.toggle('ativo');
 });
 
+window.onload = () => {
+    const list = document.getElementById('image-list');
+    const items = list.querySelectorAll('li'); // Seleciona todos os itens
+
+    // Duplicando os itens da lista para garantir um ciclo contínuo
+    items.forEach(item => {
+        const clone = item.cloneNode(true); // Clona o item
+        list.appendChild(clone); // Adiciona a cópia ao final da lista
+    });
+};
